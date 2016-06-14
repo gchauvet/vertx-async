1c7c70e94c9e79613e43b2e9e57a281ef65ddc3f
Guillaume Chauvet <gchauvet@zatarox.com>
2016-06-14 20:16:13 +0200

    Add UT for ObjectWrapper

---
+++ src/test/java/io/zatarox/vertx/async/ObjectWrapperTest.java
@@ -0,0 +1,50 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2016 Guillaume.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package io.zatarox.vertx.async;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+public final class ObjectWrapperTest {
+
+    private ObjectWrapper<Integer> instance;
+    
+    @Before
+    public void setUp() {
+        instance = new ObjectWrapper<>(73);
+    }
+    
+    @Test(timeout = 100)
+    public void testConstructor() {
+        assertEquals(73, (int) instance.getObject());
+    }
+    
+    @Test(timeout = 100)
+    public void testSetter() {
+        instance.setObject(42);
+        assertEquals(42, (int) instance.getObject());
+    }
+    
+}
