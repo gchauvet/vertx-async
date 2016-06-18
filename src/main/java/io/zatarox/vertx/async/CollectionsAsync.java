0fda8e26597aa91de7f2678080f1fd9bacc2e080
Guillaume Chauvet <gchauvet@zatarox.com>
2016-06-18 17:34:07 +0200

    Update javadoc

--- src/main/java/io/zatarox/vertx/async/CollectionsAsync.java
+++ src/main/java/io/zatarox/vertx/async/CollectionsAsync.java
@@ -36,26 +36,27 @@ public final class CollectionsAsync {
     }
 
     /**
-     * Applies the function `consumer` to each item in `iterable`, in parallel.
-     * The `consumer` is called with an item from the list, and a callback when
-     * it has finished. If the `consumer` passes an error to its `callback`, the
-     * main `handler` (for the `each` function) is immediately called with the
-     * error.
+     * Applies the function {@code consumer} to each item in {@code iterable},
+     * in parallel. The {@code consumer} is called with an item from the list,
+     * and a callback when it has finished. If the {@code consumer} passes an
+     * error to its {@code callback}, the main {@code handler} (for the
+     * {@code each} function) is immediately called with the error.
      *
-     * Note, that since this function applies `consumer` to each item in
+     * Note, that since this function applies {@code consumer} to each item in
      * parallel, there is no guarantee that the consumer functions will complete
      * in order.
      *
      * @param <T> Define the manipulated type.
      * @param instance The Vertx instance to use.
      * @param iterable A collection to iterate over.
-     * @param consumer A function to apply to each item in `iterable`. The
-     * iteratee is passed a `consumer` which must be called once it has
-     * completed. If no error has occurred, the `callback` should be run without
-     * arguments or with an explicit `null` argument. The array index is not
-     * passed to the consumer. If you need the index, use `eachOf`.
-     * @param handler A callback which is called when all `consumer` functions
-     * have finished, or an error occurs.
+     * @param consumer A function to apply to each item in {@code iterable}. The
+     * iteratee is passed a {@code consumer} which must be called once it has
+     * completed. If no error has occurred, the {@code callback} should be run
+     * without arguments or with an explicit {@code null} argument. The array
+     * index is not passed to the consumer. If you need the index, use
+     * {@code eachOf}.
+     * @param handler A callback which is called when all {@code consumer}
+     * functions have finished, or an error occurs.
      */
     public static <T> void each(final Vertx instance, final Collection<T> iterable, final BiConsumer<T, Handler<AsyncResult<Void>>> consumer, final Handler<AsyncResult<Void>> handler) {
         if (iterable.isEmpty()) {
@@ -84,19 +85,20 @@ public final class CollectionsAsync {
     }
 
     /**
-     * Like `each`, except that it passes the tuple key/value as argument to the
-     * consumer.
+     * Like {@code each}, except that it passes the tuple key/value as argument
+     * to the consumer.
      *
      * @param <K> Define type of key.
      * @param <V> Define type of value.
      * @param instance The Vertx instance to use.
      * @param iterable A collection to iterate over.
-     * @param consumer A function to apply to each item in `iterable`. The `key`
-     * is the item's key. The iteratee is passed a `handler` which must be
-     * called once it has completed. If no error has occurred, the callback
-     * should be run without arguments or with an explicit `null` argument.
-     * @param handler A callback which is called when all `consumer` functions
-     * have finished, or an error occurs.
+     * @param consumer A function to apply to each item in {@code iterable}. The
+     * {@code key} is the item's key. The iteratee is passed a {@code handler}
+     * which must be called once it has completed. If no error has occurred, the
+     * callback should be run without arguments or with an explicit {@code null}
+     * argument.
+     * @param handler A callback which is called when all {@code consumer}
+     * functions have finished, or an error occurs.
      */
     public static <K, V> void forEachOf(final Vertx instance, final Map<K, V> iterable, final BiConsumer<KeyValue<K, V>, Handler<AsyncResult<Void>>> consumer, final Handler<AsyncResult<Void>> handler) {
         if (iterable.isEmpty()) {
@@ -125,28 +127,31 @@ public final class CollectionsAsync {
     }
 
     /**
-     * Produces a new collection of values by mapping each value in `iterable`
-     * through the `consumer` function. The `consumer` is called with an item
-     * from `iterbale` and a callback for when it has finished processing. Each
-     * of these callback takes 2 arguments: an `error`, and the transformed item
-     * from `iterable`. If `consumer` passes an error to its callback, the main
-     * `handler` (for the `map` function) is immediately called with the error.
+     * Produces a new collection of values by mapping each value in
+     * {@code iterable} through the {@code consumer} function. The
+     * {@code consumer} is called with an item from {@code iterbale} and a
+     * callback for when it has finished processing. Each of these callback
+     * takes 2 arguments: an {@code error}, and the transformed item from
+     * {@code iterable}. If {@code consumer} passes an error to its callback,
+     * the main {@code handler} (for the {@code map} function) is immediately
+     * called with the error.
      *
-     * Note, that since this function applies the `consumer` to each item in
-     * parallel, there is no guarantee that the `consumer` functions will
-     * complete in order. However, the results array will be in the same order
-     * as the original `iterable`.
+     * Note, that since this function applies the {@code consumer} to each item
+     * in parallel, there is no guarantee that the {@code consumer} functions
+     * will complete in order. However, the results array will be in the same
+     * order as the original {@code iterable}.
      *
      * @param <I> Define input type.
      * @param <O> Define output type.
      * @param instance Define Vertx instance.
      * @param iterable A collection to iterate over.
-     * @param consumer A function to apply to each item in `iterable`. The
-     * iteratee is passed a `handler` which must be called once it has completed
-     * with an error and a transformed item. Invoked with (item, callback).
-     * @param handler A callback which is called when all `consumer` functions
-     * have finished, or an error occurs. Results is a List of the transformed
-     * items from the `iterable`.
+     * @param consumer A function to apply to each item in {@code iterable}. The
+     * iteratee is passed a {@code handler} which must be called once it has
+     * completed with an error and a transformed item. Invoked with (item,
+     * callback).
+     * @param handler A callback which is called when all {@code consumer}
+     * functions have finished, or an error occurs. Results is a List of the
+     * transformed items from the {@code iterable}.
      */
     public static <I, O> void map(final Vertx instance, final List<I> iterable, final BiConsumer<I, Handler<AsyncResult<O>>> consumer, final Handler<AsyncResult<Collection<O>>> handler) {
         final List<O> mapped = new ArrayList<>(iterable.size());
@@ -182,17 +187,17 @@ public final class CollectionsAsync {
     }
 
     /**
-     * Returns a new collection of all the values in `iterable` which pass an
-     * async truth test. This operation is performed in parallel, but the
+     * Returns a new collection of all the values in {@code iterable} which pass
+     * an async truth test. This operation is performed in parallel, but the
      * results array will be in the same order as the original.
      *
      * @param <T> Define the manipulated type.
      * @param instance Define Vertx instance.
      * @param iterable A collection to iterate over.
-     * @param consumer A truth test to apply to each item in `iterable`. The
-     * `consumer` is passed a `handler`, which must be called with a boolean
-     * argument once it has completed.
-     * @param handler A callback which is called after all the `consumer`
+     * @param consumer A truth test to apply to each item in {@code iterable}.
+     * The {@code consumer} is passed a {@code handler}, which must be called
+     * with a boolean argument once it has completed.
+     * @param handler A callback which is called after all the {@code consumer}
      * functions have finished.
      */
     public static <T> void filter(final Vertx instance, final Collection<T> iterable, final BiConsumer<T, Handler<AsyncResult<Boolean>>> consumer, final Handler<AsyncResult<Collection<T>>> handler) {
@@ -228,15 +233,16 @@ public final class CollectionsAsync {
     }
 
     /**
-     * The opposite of `filter`. Removes values that pass an `async` truth test.
+     * The opposite of {@code filter}. Removes values that pass an {@code async}
+     * truth test.
      *
      * @param <T> Define the manipulated type.
      * @param instance Define Vertx instance.
      * @param iterable A collection to iterate over.
-     * @param consumer A falsy test to apply to each item in `iterable`. The
-     * `consumer` is passed a `handler`, which must be called with a boolean
-     * argument once it has completed.
-     * @param handler A callback which is called after all the `consumer`
+     * @param consumer A falsy test to apply to each item in {@code iterable}.
+     * The {@code consumer} is passed a {@code handler}, which must be called
+     * with a boolean argument once it has completed.
+     * @param handler A callback which is called after all the {@code consumer}
      * functions have finished.
      */
     public static <T> void reject(final Vertx instance, final Collection<T> iterable, final BiConsumer<T, Handler<AsyncResult<Boolean>>> consumer, final Handler<AsyncResult<Collection<T>>> handler) {
@@ -252,19 +258,21 @@ public final class CollectionsAsync {
     }
 
     /**
-     * A relative of `reduce`. Takes a Collection, and iterates over each
-     * element in series, each step potentially mutating an `accumulator` value.
-     * The type of the accumulator defaults to the type of collection passed in.
+     * A relative of {@code reduce}. Takes a Collection, and iterates over each
+     * element in series, each step potentially mutating an {@code accumulator}
+     * value. The type of the accumulator defaults to the type of collection
+     * passed in.
      *
      * @param <I> Define the type of input data
      * @param <O> Define the type of output data
      * @param instance Define Vertx instance.
      * @param iterable A collection to iterate over.
      * @param consumer A function applied to each item in the collection that
-     * potentially modifies the accumulator. The `consumer` is passed a
-     * `handler`. If an error is passed to the callback, the transform is
-     * stopped and the main `handler` is immediately called with the error.
-     * @param handler A callback which is called after all the `consumer`
+     * potentially modifies the accumulator. The {@code consumer} is passed a
+     * {@code handler}. If an error is passed to the callback, the transform is
+     * stopped and the main {@code handler} is immediately called with the
+     * error.
+     * @param handler A callback which is called after all the {@code consumer}
      * functions have finished. Result is the transformed accumulator.
      */
     public static <I, O> void transform(final Vertx instance, final Collection<I> iterable, final BiConsumer<I, Handler<AsyncResult<O>>> consumer, final Handler<AsyncResult<Collection<O>>> handler) {
@@ -291,9 +299,9 @@ public final class CollectionsAsync {
     }
 
     /**
-     * A relative of `reduce`. Takes a Map, and iterates over each element in
-     * series, each step potentially mutating an `accumulator` value. The type
-     * of the accumulator defaults to the type of collection passed in.
+     * A relative of {@code reduce}. Takes a Map, and iterates over each element
+     * in series, each step potentially mutating an {@code accumulator} value.
+     * The type of the accumulator defaults to the type of collection passed in.
      *
      * @param <K> Define the type of input key.
      * @param <V> Define the type of input value.
@@ -302,10 +310,11 @@ public final class CollectionsAsync {
      * @param instance Define Vertx instance.
      * @param iterable A collection to iterate over.
      * @param consumer A function applied to each item in the collection that
-     * potentially modifies the accumulator. The `consumer` is passed a
-     * `handler`. If an error is passed to the callback, the transform is
-     * stopped and the main `handler` is immediately called with the error.
-     * @param handler A callback which is called after all the `consumer`
+     * potentially modifies the accumulator. The {@code consumer} is passed a
+     * {@code handler}. If an error is passed to the callback, the transform is
+     * stopped and the main {@code handler} is immediately called with the
+     * error.
+     * @param handler A callback which is called after all the {@code consumer}
      * functions have finished. Result is the transformed accumulator.
      */
     public static <K, V, T, R> void transform(final Vertx instance, final Map<K, V> iterable, final BiConsumer<KeyValue<K, V>, Handler<AsyncResult<KeyValue<T, R>>>> consumer, final Handler<AsyncResult<Map<T, R>>> handler) {
@@ -333,9 +342,9 @@ public final class CollectionsAsync {
     }
 
     /**
-     * Reduces `collection` into a single value using an async `consumer` to
-     * return each successive step. `memo` is the initial state of the
-     * reduction. This function only operates in series.
+     * Reduces {@code collection} into a single value using an async
+     * {@code consumer} to return each successive step. {@code memo} is the
+     * initial state of the reduction. This function only operates in series.
      *
      * For performance reasons, it may make sense to split a call to this
      * function into a parallel map, and then use the normal
@@ -349,10 +358,11 @@ public final class CollectionsAsync {
      * @param collection A collection to iterate over.
      * @param memo Initial state of the reduction.
      * @param function A function applied to each item in the array to produce
-     * the next step in the reduction. The `function` is passed a `handler)`
-     * which accepts an optional error as its first argument, and the state of
-     * the reduction as the second. If an error is passed to the callback, the
-     * reduction is stopped and the main `handler` is immediately called.
+     * the next step in the reduction. The {@code function} is passed a
+     * `handler)` which accepts an optional error as its first argument, and the
+     * state of the reduction as the second. If an error is passed to the
+     * callback, the reduction is stopped and the main {@code handler} is
+     * immediately called.
      * @param handler
      */
     public static <I, O> void reduce(final Vertx instance, final Collection<I> collection, final O memo, final BiConsumer<Pair<I, O>, Handler<AsyncResult<O>>> function, final Handler<AsyncResult<O>> handler) {
@@ -380,22 +390,22 @@ public final class CollectionsAsync {
     }
 
     /**
-     * Returns the first value in `collection` that passes an async truth test.
-     * The `function` is applied in parallel, meaning the first iteratee to
-     * return `true` will fire the detect `callback` with that result. That
-     * means the result might not be the first item in the original `collection`
-     * (in terms of order) that passes the test.
+     * Returns the first value in {@code collection} that passes an async truth
+     * test. The {@code function} is applied in parallel, meaning the first
+     * iteratee to return {@code true} will fire the detect {@code callback}
+     * with that result. That means the result might not be the first item in
+     * the original {@code collection} (in terms of order) that passes the test.
      *
      * @param <T> Define the manipulated type.
      * @param instance Define Vertx instance.
      * @param collection A collection to iterate over.
-     * @param function A truth test to apply to each item in `coll`. The
-     * iteratee is passed a `callback(err, truthValue)` which must be called
+     * @param function A truth test to apply to each item in {@code collection}.
+     * The iteratee is passed a `callback(err, truthValue)` which must be called
      * with a boolean argument once it has completed.
      * @param handler A callback which is called as soon as any iteratee returns
-     * `true`, or after all the `function` functions have finished. Result will
-     * be the first item in the array that passes the truth test (function) or
-     * the value `null` if none passed.
+     * {@code true}, or after all the {@code function} functions have finished.
+     * Result will be the first item in the array that passes the truth test
+     * (function) or the value {@code null} if none passed.
      */
     public static <T> void detect(final Vertx instance, final Collection<T> collection, final BiConsumer<T, Handler<AsyncResult<Boolean>>> function, final Handler<AsyncResult<T>> handler) {
         if (collection.isEmpty()) {
@@ -421,7 +431,7 @@ public final class CollectionsAsync {
                         handler.handle(DefaultAsyncResult.fail(event));
                     }
                 }));
-                
+
                 if (found.getObject()) {
                     break;
                 }
--- src/main/java/io/zatarox/vertx/async/FlowsAsync.java
+++ src/main/java/io/zatarox/vertx/async/FlowsAsync.java
@@ -36,24 +36,25 @@ public final class FlowsAsync {
     }
 
     /**
-     * Run the functions in the `tasks` collection in series, each one running
-     * once the previous function has completed. If any functions in the series
-     * pass an error to its callback, no more functions are run, and `handler`
-     * is immediately called with the value of the error. Otherwise, `handler`
-     * receives an array of results when `tasks` have completed.
+     * Run the functions in the {@code tasks} collection in series, each one
+     * running once the previous function has completed. If any functions in the
+     * series pass an error to its callback, no more functions are run, and
+     * {@code handler} is immediately called with the value of the error.
+     * Otherwise, {@code handler} receives an array of results when
+     * {@code tasks} have completed.
      *
      * It is also possible to use an object instead of an array. Each property
      * will be run as a function, and the results will be passed to the final
-     * `callback` as an object instead of an array.
+     * {@code callback} as an object instead of an array.
      *
      * @param <T> Define the manipulated data type.
      * @param instance Define Vertx instance.
      * @param tasks A collection containing functions to run, each function is
-     * passed a `handler` it must call on completion with an optional error and
-     * an optional `result` value.
+     * passed a {@code handler} it must call on completion with an optional
+     * error and an optional {@code result} value.
      * @param handler An optional handler to run once all the functions have
      * completed. This function gets a results array (or object) containing all
-     * the result arguments passed to the `task` handlers.
+     * the result arguments passed to the {@code task} handlers.
      */
     public static <T> void series(final Vertx instance, Collection<Consumer<Handler<AsyncResult<T>>>> tasks, final Handler<AsyncResult<List<T>>> handler) {
         final Iterator<Consumer<Handler<AsyncResult<T>>>> iterator = tasks.iterator();
@@ -85,23 +86,24 @@ public final class FlowsAsync {
     }
 
     /**
-     * Attempts to get a successful response from `task` no more than `times`
-     * times before returning an error. If the task is successful, the
-     * `callback` will be passed the result of the successful task. If all
-     * attempts fail, the callback will be passed the error and result (if any)
-     * of the final attempt.
+     * Attempts to get a successful response from {@code task} no more than
+     * {@code times} times before returning an error. If the task is successful,
+     * the {@code callback} will be passed the result of the successful task. If
+     * all attempts fail, the callback will be passed the error and result (if
+     * any) of the final attempt.
      *
      * @param <T> Define the manipulated data type.
      * @param instance Define Vertx instance.
      * @param task A function which receives two arguments: (1) a `callback(err,
-     * result)` which must be called when finished, passing `err` (which can be
-     * `null`) and the `result` of the function's execution, and (2) a `results`
-     * object, containing the results of the previously executed functions (if
-     * nested inside another control flow).
+     * result)` which must be called when finished, passing {@code err} (which
+     * can be {@code null}) and the {@code result} of the function's execution,
+     * and (2) a {@code results} object, containing the results of the
+     * previously executed functions (if nested inside another control flow).
      * @param times The number of attempts to make before giving up.
      * @param handler An optional callback which is called when the task has
-     * succeeded, or after the final failed attempt. It receives the `err` and
-     * `result` arguments of the last attempt at completing the `task`.
+     * succeeded, or after the final failed attempt. It receives the {@code err}
+     * and {@code result} arguments of the last attempt at completing the
+     * {@code task}.
      */
     public static <T> void retry(final Vertx instance, final Consumer<Handler<AsyncResult<T>>> task, final long times, final Handler<AsyncResult<T>> handler) {
         instance.runOnContext((Void) -> {
@@ -128,15 +130,15 @@ public final class FlowsAsync {
     }
 
     /**
-     * Calls the asynchronous function `task` with a callback parameter that
-     * allows it to call itself again, in series, indefinitely. If an error is
-     * passed to the callback then `handler` is called with the error, and
-     * execution stops, otherwise it will never be called.
+     * Calls the asynchronous function {@code task} with a callback parameter
+     * that allows it to call itself again, in series, indefinitely. If an error
+     * is passed to the callback then {@code handler} is called with the error,
+     * and execution stops, otherwise it will never be called.
      *
      * @param <T> Define the manipulated data type.
      * @param instance Define Vertx instance.
      * @param task A function to call repeatedly.
-     * @param handler when `task` passes an error to it's callback, this
+     * @param handler when {@code task} passes an error to it's callback, this
      * function will be called, and execution stops.
      */
     public static <T> void forever(final Vertx instance, final Consumer<Handler<AsyncResult<T>>> task, final Handler<AsyncResult<T>> handler) {
@@ -155,16 +157,16 @@ public final class FlowsAsync {
     }
 
     /**
-     * Runs the `tasks` array of functions in series, each passing their results
-     * to the next in the array. However, if any of the `tasks` pass an error to
-     * their own callback, the next function is not executed, and the main
-     * `callback` is immediately called with the error.
+     * Runs the {@code tasks} array of functions in series, each passing their
+     * results to the next in the array. However, if any of the {@code tasks}
+     * pass an error to their own callback, the next function is not executed,
+     * and the main {@code callback} is immediately called with the error.
      *
      * @param <I> Define input data type of functions
      * @param <O> Define ouput data type of functions
      * @param instance Define Vertx instance.
      * @param tasks An array of functions to run, each function is passed with
-     * previously computed result thougth the `handler`.
+     * previously computed result thougth the {@code handler}.
      * @param handler Handler to run once all the functions have completed. This
      * will be passed the results of the last task's callback.
      */
