2b4d7b12850f13a90921fb8a587d8ad2bfb051f2
Guillaume Chauvet <gchauvet@zatarox.com>
2016-07-17 11:03:06 +0200

    Improve coverage

--- src/test/java/io/zatarox/vertx/async/AsyncFactorySingletonTest.java
+++ src/test/java/io/zatarox/vertx/async/AsyncFactorySingletonTest.java
@@ -15,13 +15,45 @@
  */
 package io.zatarox.vertx.async;
 
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.RunTestOnContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import io.zatarox.vertx.async.api.*;
 import static org.junit.Assert.*;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(VertxUnitRunner.class)
 public final class AsyncFactorySingletonTest {
 
+    @Rule
+    public RunTestOnContext rule = new RunTestOnContext();
+
     @Test
-    public void onlyOneInstance() {
+    public void onlyOneFactoryInstance(final TestContext context) {
         assertSame(AsyncFactorySingleton.getInstance(), AsyncFactorySingleton.getInstance());
     }
+
+    @Test
+    public void differentAsyncFlowsInstance(final TestContext context) {
+        final AsyncFlows instance1 = AsyncFactorySingleton.getInstance().createFlows(rule.vertx().getOrCreateContext());
+        final AsyncFlows instance2 = AsyncFactorySingleton.getInstance().createFlows(rule.vertx().getOrCreateContext());
+        assertNotSame(instance1, instance2);
+    }
+    
+    @Test
+    public void differentAsyncCollectionsInstance(final TestContext context) {
+        final AsyncCollections instance1 = AsyncFactorySingleton.getInstance().createCollections(rule.vertx().getOrCreateContext());
+        final AsyncCollections instance2 = AsyncFactorySingleton.getInstance().createCollections(rule.vertx().getOrCreateContext());
+        assertNotSame(instance1, instance2);
+    }
+    
+    @Test
+    public void differentAsyncUtilsInstance(final TestContext context) {
+        final AsyncUtils instance1 = AsyncFactorySingleton.getInstance().createUtils(rule.vertx().getOrCreateContext());
+        final AsyncUtils instance2 = AsyncFactorySingleton.getInstance().createUtils(rule.vertx().getOrCreateContext());
+        assertNotSame(instance1, instance2);
+    }
+
 }
